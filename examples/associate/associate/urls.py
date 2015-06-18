2eb65930b6f23537a9f872920c1f34bd98984215
ondrejskowronek <ondrejskowronek@abdoc.net>
2015-06-18 13:56:20 +0200

    Init - refactoring library, deleting everything what is not needed

--- examples/associate/associate/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    (r'^$', 'associate.views.associate'),
-    (r'^associate$', 'associate.views.display_associate'),
-    (r'^disassociate$', 'associate.views.display_disassociate'),
-    (r'^openid/', include('django_mojeid.urls')),
-)
